565905960894d3fe7384c27df77153d768dbe438
Alexis <alexis.thomas1795@gmail.com>
2025-08-02 14:49:29 -0500

    Step 2 - Exercises completed

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ pet.age = 6;
 console.log(pet.age);
 
 // TODO: What do you think each console.log will print?
+
+const book = {
+    "title": "JavaScript for Beginners",
+    "author": "Jane Doe",
+    "pages": 250,
+    "isPublished": true
+};
+
+// TODO: Name labels everything on left side
+// TODO: Everything on Right side
+
+const pet = {
+    "name": "Buddy",
+    "species": "Dog",
+    "age": 5
+};
+
+console.log(pet);
+
+// Values Buddy, Dog, 5
+
+const sprite = {
+    "name": "Hero",
+    "x": 100,
+    "y": 200
+};
+
+console.log(sprite);
+
+// TODO: Name of the sprite is Hero and x/y are the coordinates
+
+const car = {
+    "make": "Toyota",
+    "model": "Corolla",
+    "year": 2020,
+    "color": "Blue"
+};
+
+// TODO: Fix the syntax error.
+// TODO: Explain what was wrong in a comment.
+console.log(car);
